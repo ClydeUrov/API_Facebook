60dd230fc677592bc5c1b5dabbf930ce9c276670
Владислав Юров <clydeurov@gmail.com>
2022-10-30 20:12:08 +0200

    Update upload_comics.py

--- upload_comics.py
+++ upload_comics.py
@@ -1,32 +1,35 @@
-import os
-import requests
-import facebook
-from dotenv import load_dotenv
-import time
-import random
 import logging
+import os
+import random
+import time
+
+import facebook
+import requests
+from dotenv import load_dotenv
 
 
 def main():
     load_dotenv()
 
-    group=os.environ["GROUP_ID"]
-    graph=facebook.GraphAPI(access_token=os.environ["FACEBOOK_TOKEN"])
+    group = os.environ["GROUP_ID"]
+    graph = facebook.GraphAPI(access_token=os.environ["FACEBOOK_TOKEN"])
 
     used = []
     pages = 2691
     while len(used) != pages:
         try:
-            number = random.randint(0,pages)
+            number = random.randint(0, pages)
             if number not in used:
                 used.append(number)
                 response = requests.get(
                     f"https://xkcd.com/{number}/info.0.json"
                 )
                 response.raise_for_status()
-                message = response.json()['alt']
-                image = response.json()['img']
-                graph.put_object(group, 'feed', message=message, link=image)
+                message = response.json()["alt"]
+                image = response.json()["img"]
+                graph.put_object(
+                    group, "feed", message=message, link=image
+                )
                 time.sleep(86400)
         except requests.ConnectionError:
             time.sleep(30)
@@ -34,5 +37,6 @@ def main():
             logging.warning("Бот упал с ошибкой:")
             logging.warning(err, exc_info=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
