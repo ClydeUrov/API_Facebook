48daee70c0479bf40a9f9809f5b195ab24b7fb38
Владислав Юров <clydeurov@gmail.com>
2022-11-09 15:49:25 +0200

    Moved loading to a separate function and removed unnecessary features.

--- upload_comics.py
+++ upload_comics.py
@@ -1,53 +1,38 @@
 import logging
-import argparse
 import os
 import random
-import time
 
 import facebook
 import requests
 from dotenv import load_dotenv
 
 
-def main(hours):
-    load_dotenv()
-
-    group = os.environ["GROUP_ID"]
+def upload_to_facebook(message, image):
     graph = facebook.GraphAPI(access_token=os.environ["FACEBOOK_TOKEN"])
+    graph.put_object(
+            os.environ["GROUP_ID"], "feed", message=message, link=image
+        )
 
-    used = []
-    pages = 2691
-    while len(used) != pages:
-        try:
-            number = random.randint(0, pages)
-            if number not in used:
-                used.append(number)
-                response = requests.get(
-                    f"https://xkcd.com/{number}/info.0.json"
-                )
-                response.raise_for_status()
-                message = response.json()["alt"]
-                image = response.json()["img"]
-                graph.put_object(
-                    group, "feed", message=message, link=image
-                )
-                time.sleep(hours * 60)
-        except requests.ConnectionError:
-            time.sleep(30)
-        except Exception as err:
-            logging.warning("Бот упал с ошибкой:")
-            logging.warning(err, exc_info=True)
+
+def main():
+    load_dotenv()
+    try:
+        response = requests.get('https://xkcd.com/info.0.json')
+        response.raise_for_status()
+        total_comics = response.json()["num"]
+
+        comic_number = random.randrange(total_comics)
+        response = requests.get(
+            f"https://xkcd.com/{comic_number}/info.0.json"
+        )
+        response.raise_for_status()
+        comic_book = response.json()
+
+        upload_to_facebook(comic_book["alt"], comic_book["img"])
+
+    except Exception:
+        logging.exception(msg='Бот упал с ошибкой:')
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Выгружает комиксы в группу в Facebook."
-    )
-    parser.add_argument(
-        "-t",
-        dest="hours",
-        help="Частота выгрузки комиксов. [часы]",
-        default="24",
-        type=int,
-    )
-    main(parser.parse_args().hours)
+    main()
